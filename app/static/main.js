91d269c08448bddfb4a665c52aea72d0943342d5
Ondřej <ondra-oc@seznam.cz>
2022-09-08 07:46:57 +0200

    styled main

--- app/static/main.js
+++ app/static/main.js
@@ -4,5 +4,5 @@ let proc_jsou = document.querySelector('#proc_jsou');
 
 proc_jsou.addEventListener('click', () => {
     scriptos_content.style.backgroundColor = "#00FF00";
-    scriptos_content.innerHTML = "<iframe id='myHtml' src='{{show('proc_jsou')}}'></iframe>";
+    scriptos_content.innerHTML = "<iframe src='proc_jsou' frameborder='0' scrolling='no' style='width: 100%; height= 100%;'>";
 })
