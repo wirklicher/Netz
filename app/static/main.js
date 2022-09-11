92480f2d974759ff1277b544dd31e33b07bf2b9b
Ondřej <ondra-oc@0seznam.cz>
2022-09-11 19:15:32 +0200

    adaptive design has been improved

--- app/static/main.js
+++ app/static/main.js
@@ -2,6 +2,7 @@ let scriptos_content = document.querySelector('#main');
 let proc_jsou = document.querySelector('#proc_jsou');
 
 
+
 proc_jsou.addEventListener('click', () => {
     scriptos_content.style.backgroundColor = "#00FF00";
     scriptos_content.innerHTML = "<iframe src='proc_jsou' frameborder='0' scrolling='no' style='width: 100%; height= 100%;'>";
