ad75801b12f44bd3e9e2e7e83cc5d29982cae4e7
Ondřej <ondra-oc@0seznam.cz>
2022-09-18 15:49:29 +0200

    major changes

--- app/static/main.js
+++ app/static/main.js
@@ -1,8 +1,17 @@
 let scriptos_content = document.querySelector('#main');
 let proc_jsou = document.querySelector('#proc_jsou');
+let vlan = document.querySelector('#vlan')
+
+
+
 
 
 
 proc_jsou.addEventListener('click', () => {
-    scriptos_content.innerHTML = "<iframe src='vlan' frameborder='0' scrolling='no' style='width: 100%; height= 100%;'>";
+    let a = 1;
 })
+
+vlan.addEventListener('click', () => {
+    scriptos_content.innerHTML = "<iframe id='iframe1' src='vlan' frameborder='0' scrolling='no' style='width: 100%; height: 100%; overflow:hidden;'>";
+})
+
