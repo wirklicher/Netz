09625d0384b02e4d72b62a550b69c8be4a69fce2
Ondřej <ondra-oc@0seznam.cz>
2022-10-09 21:10:48 +0200

    weekly update

---
+++ app/static/quest.js
@@ -0,0 +1,18 @@
+if (typeof MCQS === "undefined"){
+    MCQS = [{
+        question: "Je Connor hot?",
+        choice1: "Ano",
+        choice2: "Jasnacka",
+        choice3: "Ovšem",
+        choice4: "Connor jo?",
+        answer: 1,
+    },
+    {
+        question: "Mas rad UwU?",
+        choice1: "Ano",
+        choice2: "Jasnacka",
+        choice3: "Ovšem",
+        choice4: "UwU!!!",
+        answer: 3, 
+    }]
+}
--- app/static/quiz.js
+++ app/static/quiz.js
@@ -8,6 +8,10 @@ if(typeof play === 'undefined'){
     play = document.querySelector('#play');
 }
 
+if(typeof result === 'undefined'){
+    result = document.querySelector('.result');
+}
+
 // Quiz
 if(typeof quiz === 'undefined'){
     quiz = document.querySelector('#quiz');
@@ -15,14 +19,16 @@ if(typeof quiz === 'undefined'){
 
 //Question Section
 if(typeof questNo === 'undefined'){
-    questNo = document.querySelector('#questNo');
+    questNo = document.querySelector('#questionNo');
 }
 if(typeof questText === 'undefined'){
-    questText = document.querySelector('#questText');
+    questText = document.querySelector('#questionText');
 }
 if(typeof time === 'undefined'){
     time = document.querySelector('#time');
 }
+
+
 
 
 //Multiple Choices Of Questions
@@ -51,7 +57,8 @@ if(typeof next_quest === 'undefined'){
 
 //Get All H4 From Quiz Sector
 if(typeof choice_que === 'undefined'){
-    choice_que = document.querySelector('.choice_que');
+    choice_que = document.querySelectorAll('.choice_que');
+    
 }
 
 
@@ -77,13 +84,111 @@ if(typeof UserAns === 'undefined'){
     UserAns = undefined;
 }
 
+if(typeof points === 'undefined'){
+    points = document.querySelector("#points")
+}
 
-//Clicked Start Button
-play.addEventListener('click', ()=>{
-    btns_quiz.style.display = 'none';
+
+
+
+    play.addEventListener('click', ()=>{
+    btns_quiz.style.display = "none";
     quiz.style.display = 'block';
+
+    interval = setInterval(countDown, 1000);
+    loadData();
 })
 
+if (typeof countDown === "undefined"){
+     countDown = ()=>{
+        if(timer === 20){
+            clearInterval(interval);
+        }
+        else {
+            timer++;
+            time.innerText = timer;
+            //console.log(timer);
+        }
+    }
+}
+
+
+if(typeof loadData === 'undefined'){
+    loadData = ()=>{
+        questNo.innerText = index + 1 + ". ";
+        questText.innerText = MCQS[index].question;
+        opt1.innerText = MCQS[index].choice1;
+        opt2.innerText = MCQS[index].choice2;
+        opt3.innerText = MCQS[index].choice3;
+        opt4.innerText = MCQS[index].choice4;
+    
+        timer = 0;
+    }
+}
+
+
+choice_que.forEach((choices, choiceNo) => {
+    choices.addEventListener("click", ()=>{
+        choices.classList.add("active");
+
+        if(choiceNo === MCQS[index].answer)
+        {
+            correct++;
+            console.log(correct);
+        }
+        else {
+            correct += 0;
+        }
+
+        clearInterval(interval);
+
+    for(i = 0; i <= 3; i++){
+        choice_que[i].classList.add("disabled");
+    }
+    })
+
+    
+});
+
+next_quest.addEventListener("click", ()=> {
+
+    console.log(index);
+    console.log(MCQS.length - 1);
+
+    if(index !== MCQS.length - 1){
+        index++;
+        choice_que.forEach(removeActive => {
+            removeActive.classList.remove("active");
+            removeActive.classList.remove("disabled");
+        })
+
+        loadData();
+
+        total_correct.innerHTML = `${correct} Out Of ${MCQS.length} Questions`;
+        clearInterval(interval);
+        interval = setInterval(countDown, 1000);
+        
+    }
+    
+    else {
+        index = 0;
+
+        clearInterval(interval);
+        quiz.style.display = "none";
+        points.innerHTML = `You got ${correct} Out Of ${MCQS.length}`;
+        result.style.display = "block";
+    }
+    
+    for (i = 0; i <= 3; i++){
+        choice_que[i].classList.remove("disabled");
+    }
+})
+
+
+
+//Clicked Start Button
+
+ 
 
 
 
